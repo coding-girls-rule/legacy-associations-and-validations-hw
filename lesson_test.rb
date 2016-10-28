76365fbe98574c734bc5c87f078faa0a4bec91eb
barnameh <farimahd@gmail.com>
2016-10-28 15:08:55 -0400

    Added validations and defined tests for verification.

--- lesson_test.rb
+++ lesson_test.rb
@@ -46,6 +46,13 @@ class LessonTest < Minitest::Test
     assert_equal pre_assmt, lesson_12.pre_class_assignment
   end
 
+  def test_lesson_has_in_class_assignment
+    lesson_12 = Lesson.create!(name: "Setting the stage", description:"We learn how to set the stage for maximum effect")
+    in_class_assingment1 = Assignment.create!(name: "Chapter One")
+    lesson_12.in_class_assignment = in_class_assingment1
+    assert_equal in_class_assingment1, lesson_12.in_class_assignment
+  end
+
   def test_deleting_a_lesson_destroys_related_readings
     reading_12a = Reading.create!(caption: "Creating fog", url: "fogmachine.com", order_number: 12)
     reading_12b = Reading.create!(caption: "Setting props", url: "props.com", order_number: 12)
---
+++ user_test.rb
@@ -0,0 +1,65 @@
+# Basic test requires
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require './migration'
+require './user'
+
+
+# Overwrite the development database connection with a test connection.
+ActiveRecord::Base.establish_connection(
+  adapter:  'sqlite3',
+  database: 'test.sqlite3'
+)
+
+# Gotta run migrations before we can run tests.  Down will fail the first time,
+# so we wrap it in a begin/rescue.
+begin ApplicationMigration.migrate(:down); rescue; end
+ApplicationMigration.migrate(:up)
+
+
+# Finally!  Let's test the thing.
+class UserTest < Minitest::Test
+
+  def setup
+    User.delete_all
+  end
+
+  def test_truth
+    assert true
+  end
+
+  def test_can_create_a_user
+    user1 = User.create!(first_name: "Adam", last_name: "Smith", email: "adamsmithy@aol.com")
+    assert_equal "adamsmithy@aol.com", user1.email
+  end
+
+  def test_user_first_name_is_required
+    user1 = User.new(last_name: "Smith", email: "adamsmithy@aol.com")
+    refute user1.save
+  end
+
+  def test_user_last_name_is_required
+    user1 = User.new(first_name: "Adam", email: "adamsmithy@aol.com")
+    refute user1.save
+  end
+
+  def test_user_email_is_required
+    user1 = User.new(first_name: "Adam", last_name: "Smith")
+    refute user1.save
+  end
+
+  def test_email_uniqueness
+    User.create!(first_name: "Adam", last_name: "Smith", email: "adamsmithy@aol.com")
+    assert_raises do
+       User.create!(first_name: "Joe", last_name: "White", email: "adamsmithy@aol.com")
+     end
+  end
+
+  def test_email_has_appropriate_form
+    assert_raises do
+       User.create!(first_name: "Joe", last_name: "White", email: "adamsmithy-aol.com")
+     end
+  end
+
+end
